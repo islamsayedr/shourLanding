f8aed7243aa417076437c868d4a43ebb29c182b6
IslamSayed <islamsayedr@gmail.com>
2023-11-24 14:30:11 +0200

    other services section done

--- src/components/sections/Services.jsx
+++ src/components/sections/Services.jsx
@@ -1,3 +1,33 @@
+import serviceData from "../serviceData.js";
+
 export default function Services() {
-  return <div>Services</div>;
+  return (
+    <section className="services">
+      <div className="head">
+        <span>المزيد</span>
+        <h2>تعرف على خدماتنا الاخرى</h2>
+      </div>
+      <div className="serviceContainer">
+        {serviceData.map((service) => (
+          <Service
+            key={service.id}
+            iconURL={service.iconURL}
+            title={service.title}
+            des={service.des}
+          />
+        ))}
+      </div>
+    </section>
+  );
+}
+function Service({ iconURL, title, des }) {
+  return (
+    <div className="service">
+      <img src={iconURL} />
+      <div>
+        <h3>{title}</h3>
+        <p>{des}</p>
+      </div>
+    </div>
+  );
 }
