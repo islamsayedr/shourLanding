1036371f9d47f9e41c5a1e23a62155374dc75a93
IslamSayed <islamsayedr@gmail.com>
2023-11-24 15:49:33 +0200

    -MVP-

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -1,3 +1,15 @@
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <section className="footer">
+      <div className="content">
+        <img src="./logoWhite.svg" alt="logo" />
+        <p>
+          رسالتنا هى توفير كل الادوات التى تحتاجها لاتخاذ قرارات استثمارية ذكية
+          تحقق اهدافك المالية.
+        </p>
+      </div>
+      <span></span>
+      <div className="copyrights">©جميع الحقوق محفوظه لشور 2023</div>
+    </section>
+  );
 }
--- src/components/sections/Header.jsx
+++ src/components/sections/Header.jsx
@@ -3,7 +3,7 @@ import Btn from "../base/Btn"
 export default function Header() {
   return (
     <header>
-      <img src="./logoColored.png" alt="Shour logo" />
+      <img src="./logoColored.svg" alt="Shour logo" />
       <div className="actions">
         <Btn text="انشاء حساب" icon="" link="#" type="p" size="m"/>
         <Btn text="تسجيل دخول" icon="" link="#" type="s" size="m"/>
